542cb706716d4edba61328919c148abb07406a30
ainnot <stkim2007@gmail.com>
2024-07-17 11:43:07 +0900

    wfwefw

--- Assets/Scripts/EventActions/ComEvent.cs
+++ Assets/Scripts/EventActions/ComEvent.cs
@@ -1,17 +1,38 @@
 using UnityEngine;
+using System.Collections;
 
 public class ComEvent : EventAction
 {
+    [SerializeField] GameObject blackPanel;
     public override string Id => "com";
+    
+    public bool actionEnd = false, canClean;
+    public override bool StayActive => true;
 
     public override Vector2 Pos => new(45, 3);
 
     public override bool EndAction()
     {
-        return false;
+        if (Activated) {
+            return actionEnd;
+        } else {
+            return false;
+        }
     }
-
     public override void StartAction()
     {
+        actionEnd = false;
+
+        StartCoroutine(act());
+    }
+    IEnumerator act() {
+        canClean = false;
+
+        ActNpc.Comment("아이코 실수 ~");
+        blackPanel.SetActive(false);
+        yield return new WaitForSeconds(1f);
+        canClean = true;
+
+        GameManager.Instance.health += 10;
     }
 }
